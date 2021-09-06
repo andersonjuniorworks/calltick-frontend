79eedd7fb50f1bfe3539c8c9f604af2afdcd830b
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-09-06 12:22:42 -0300

    style: estilizando home page

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeModule } from './../home/home.module';
 import { DashboardComponent } from './dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,8 +6,20 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
-  }
+    redirectTo: 'home',
+    pathMatch: "full",
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../../pages/home/home.module').then( m => m.HomeModule)
+      },
+    ]
+  },
+
 ];
 
 @NgModule({
