f63806808c5bfc32d6734c308e9c5accab379e0a
andersonjunior.works <andersonjunior.tech@gmail.com>
2021-09-18 01:35:53 -0300

    fix: corrigindo requisição de navegação

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -45,7 +45,7 @@ export class TicketService {
   }
 
   public notification() {
-    return this.http.get(`http://localhost:5050/notify`).pipe(take(1));
+    return this.http.get(`http://187.19.165.178:5050/notify`).pipe(take(1));
   }
 
   public finish(ticket) {
