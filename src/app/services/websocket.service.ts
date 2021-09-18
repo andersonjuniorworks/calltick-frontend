041761cc976cf85602acfee30de07d7faa00ba51
andersonjunior.works <andersonjunior.tech@gmail.com>
2021-09-17 22:34:10 -0300

    feature: mudando socket

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -5,7 +5,7 @@ import { Stomp } from '@stomp/stompjs';
 
 @Injectable()
 export class WebSocketService {
-  private readonly API = `${environment.baseUrl}`;
+  private readonly API = `http://187.19.165.178:5050/api`;
   public connect() {
     let socket = new SockJS(`${this.API}`);
     let stompClient = Stomp.over(socket);
