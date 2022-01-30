467eda636d5c700bab72fa329bf1280099191a19
andersonjunior.tech <andersonjunior.tech@gmail.com>
2022-01-30 20:08:34 -0300

    feature: realizando listagem para todos os usuários ao finalizar chamado

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -218,7 +218,7 @@ export class TicketComponent implements OnInit {
           'SUCESSO!',
           `Chamado finalizado com sucesso!!!`
         );
-        this.onList();
+        this.ticketService.getTickets().subscribe(() => {});
       },
       (error) => {
         let err = error;
